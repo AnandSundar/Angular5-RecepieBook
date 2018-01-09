bfc5aa2dfb0390e96093e2b3aae91bc3b68b1836
AnandSundar <anandsundar96@gmail.com>
2018-01-09 17:03:46 -0500

    creating and outputting a shopping list edit section

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredint } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients:Ingredint[] = [
+    new Ingredint('Apples', 5),
+    new Ingredint('Tomatos', 10),
+  ];
   constructor() { }
 
   ngOnInit() {
